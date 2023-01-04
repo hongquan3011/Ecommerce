9b7d214219f766417d0c5d6ad57d397e7e01d322
hongquan3011 <hongquan0165@gmail.com>
2023-01-05 00:12:44 +0700

    second commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,18 +20,51 @@ window.onscroll = () => {
     }
 }
 //slide ảnh 
-$(document).ready(function () {
+$('#banner').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    lazyLoad:true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: false,
+    items: 1,
+    dots: true,
+    dotsEach: true,
+});
 
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: true,
-        autoplay: false,
-        autoplayTimeout: 5000,
-        autoplayHoverPause: false,
-        items: 1,
-        dots:true,
-        dotsEach:true,
-    })
+$('#product').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: true,
+    navText: ["<div class='nav-button owl-prev'>‹</div>", "<div class='nav-button owl-next'>›</div>"],
+    responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 3
+        },
+        1000: {
+          items: 5
+        }
+      },
+});
 
+$('#product-trend').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: true,
+    navText: ["<div class='nav-button owl-prev'>‹</div>", "<div class='nav-button owl-next'>›</div>"],
+    responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 3
+        },
+        1000: {
+          items: 5
+        }
+      }
 });
